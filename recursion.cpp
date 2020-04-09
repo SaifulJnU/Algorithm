9d345287ee98691a1fc80ab8db0a2d0dbe96dd60
SaifulJnU <47039014+SaifulJnU@users.noreply.github.com>
2020-04-09 21:09:07 +0600

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int x)
+{
+    if(x==0) return; ///base case
+    x--;
+    cout<<x<<endl;
+    fun(x);          ///fun() calling itself which is called recursive function
+    cout<<x<<endl;   ///this cout will work like backtracking
+    // cout<<x<<endl;
+}
+int main()
+{
+    fun(3); ///function calling
+    return 0;
+}
